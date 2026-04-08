fdf1845161545dfd936f969dde89cf0ae3a67064
Francesca Swystun <fswystun@gmail.com>
2026-04-08 12:41:52 +0100

    Set api key as environment variable for ease of use

--- Consts.cs
+++ Consts.cs
@@ -1,6 +1,6 @@
 public static class Consts { 
 
-	public static string CATAPI_KEY = "YOUR_API_KEY";
+	public static string CATAPI_KEY = Environment.GetEnvironmentVariable("MY_API_KEY");
     public static string CATAPI_ENDPOINT = "https://api.thecatapi.com/v1/";
     public static string CATAPI_VIEWS_KEYWORD = "Views";
 }
